c081e94f2b0f0a1c3eb31a5a0d7f711ec1b7c248
Jorge Sandoval <jorgeivansandoval@gmail.com>
2024-10-08 19:23:35 -0500

    video player

--- app/src/main/java/com/example/livesound/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/livesound/ui/player/PlayerViewModel.kt
@@ -1,13 +1,25 @@
 package com.example.livesound.ui.player
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class PlayerViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is player Fragment"
+class PlayerViewModel(application: Application): AndroidViewModel(application) {
+    private val exoPlayer: androidx.media3.exoplayer.ExoPlayer by lazy {
+        androidx.media3.exoplayer.ExoPlayer.Builder(application).build()
     }
-    val text: LiveData<String> = _text
+
+    fun getPlayer(): androidx.media3.exoplayer.ExoPlayer {
+        return exoPlayer
+    }
+
+    fun prepareAndPlay(uri: String) {
+        val mediaItem = androidx.media3.common.MediaItem.fromUri(uri)
+        exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.prepare()
+        exoPlayer.play()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        exoPlayer.release()
+    }
 }
