955c364344d2210312ea83559066e8af22fc59b0
andres.narvaez <dg.andres.narvaez@gmail.com>
2024-10-08 21:38:22 -0500

    Adding playlist fragment logic and layouts

--- app/src/main/java/com/example/livesound/ui/playlist/PlaylistViewModel.kt
+++ app/src/main/java/com/example/livesound/ui/playlist/PlaylistViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class PlaylistViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is playlist Fragment"
+        value = "Favoritos Weekend"
     }
     val text: LiveData<String> = _text
 }
